c224fd00ef00a3bd04f69bf54433605aa5990d41
aqibshabir <aqibfaisalshabir@gmail.com>
2023-11-29 15:47:51 +0000

    updated styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,37 @@ import "./app.css";
 
 function App() {
   return (
-    <div className="calculator-grid">
-      <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+    <div className="container">
+      <div className="calculator-container">
+        <div className="calculator-grid">
+          <div className="output">
+            <div className="previous-operand">12345</div>
+            <div className="current-operand">12345</div>
+          </div>
+          <button id="clear-buttons" className="span-two">
+            AC
+          </button>
+          <button id="clear-buttons">DEL</button>
+          <button id="operator"> ÷ </button>
+          <button> 7 </button>
+          <button> 8 </button>
+          <button> 9 </button>
+          <button id="operator"> x </button>
+          <button> 4 </button>
+          <button> 5 </button>
+          <button> 6 </button>
+          <button id="operator"> + </button>
+          <button> 1 </button>
+          <button> 2 </button>
+          <button> 3 </button>
+          <button id="operator"> - </button>
+          <button className="bottom-left"> . </button>
+          <button className="span-two"> 0 </button>
+          <button id="operator" className="bottom-right">
+            =
+          </button>
+        </div>
       </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      <button> ÷ </button>
-      <button> 1 </button>
-      <button> 2 </button>
-      <button> 3 </button>
-      <button> x </button>
-      <button> 4 </button>
-      <button> 5 </button>
-      <button> 6 </button>
-      <button> + </button>
-      <button> 7 </button>
-      <button> 8 </button>
-      <button> 9 </button>
-      <button> - </button>
-      <button> . </button>
-      <button> 0 </button>
-      <button className="span-two"> = </button>
     </div>
   );
 }
